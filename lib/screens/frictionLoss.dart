861ff35da0ac1a59acbc131abc00f8d64a5e4855
ilaydaily <119571684+ilaydaily@users.noreply.github.com>
2023-09-07 10:23:32 +0300

    sürtünme kaybı için tasarımdan önce taslak oluşturuldu

---
+++ lib/screens/frictionLoss.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class FrictionLossScreen extends StatefulWidget {
+  @override
+  _FrictionLossScreenState createState() => _FrictionLossScreenState();
+}
+
+class _FrictionLossScreenState extends State<FrictionLossScreen> {
+  TextEditingController sicaklikController = TextEditingController();
+  TextEditingController debiController = TextEditingController();
+  TextEditingController boruCapiController = TextEditingController();
+  TextEditingController boruUzunluguController = TextEditingController();
+
+  double result = 0.0;
+  String errorMessage = '';
+
+  List<String> sicaklikUnitOptions = ['C', 'F'];
+  List<String> debiUnitOptions = ['m³/h', 'l/s'];
+  List<String> boruCapiUnitOptions = ['mm', 'cm'];
+  List<String> boruUzunluguUnitOptions = ['m', 'ft'];
+
+  String sicaklikUnit = 'C';
+  String debiUnit = 'm³/h';
+  String boruCapiUnit = 'mm';
+  String boruUzunluguUnit = 'm';
+
+  void _calculateNhidrolik() {
+    double sicaklik = double.tryParse(sicaklikController.text) ?? 0.0;
+    double debi = double.tryParse(debiController.text) ?? 0.0;
+    double cap = double.tryParse(boruCapiController.text) ?? 0.0;
+    double uzunluk = double.tryParse(boruUzunluguController.text) ?? 0.0;
+
+    if (sicaklik == 0 || debi == 0 || cap == 0 || uzunluk == 0) {
+      setState(() {
+        errorMessage = 'Lütfen tüm alanları doldurun.';
+        result = 0.0;
+      });
+    } else {
+      setState(() {
+        errorMessage = '';
+        dynamic calcResult = PumpEfficiency.calcColebrook(
+          double.parse(sicaklikController.text),
+          double.parse(debiController.text),
+          double.parse(boruCapiController.text),
+          double.parse(boruUzunluguController.text),
+        );
+
+        if (calcResult is double) {
+          result = calcResult;
+        } else {
+          errorMessage = 'Hesaplama hatası: Geçersiz sonuç türü.';
+          result = 0.0;
+        }
+      });
+    }
+  }
+
+  void updateResultOnChange() {
+    _calculateNhidrolik();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Sürtünme Kaybı Hesaplama',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: sicaklikController,
+                onChanged: (value) => updateResultOnChange(),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Sıcaklık ($sicaklikUnit)'),
+              ),
+              TextField(
+                controller: debiController,
+                onChanged: (value) => updateResultOnChange(),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Debi ($debiUnit)'),
+              ),
+              TextField(
+                controller: boruCapiController,
+                onChanged: (value) => updateResultOnChange(),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Boru Çapı ($boruCapiUnit)'),
+              ),
+              TextField(
+                controller: boruUzunluguController,
+                onChanged: (value) => updateResultOnChange(),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Boru Uzunluğu ($boruUzunluguUnit)'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  var temperature = double.parse(sicaklikController.text);
+                  var flow = double.parse(debiController.text);
+                  var diameter = double.parse(boruCapiController.text) / 1000;
+                  var b_len = double.parse(boruUzunluguController.text);
+
+                  var a = math.pi * math.pow(diameter, 2) / 4;
+                  var u = flow / 3600 / a;
+
+                  print(a);
+                  print(u);
+
+                  var colebrook = PumpEfficiency.calcColebrook(
+                      diameter, .00004, u, temperature
+                  );
+
+                  print(colebrook);
+
+                  var headLoss = (colebrook["fd"] * b_len * math.pow(u, 2)) /
+                      (diameter * (2 * 9.7905));
+
+                  print(headLoss);
+                },
+                child: Text('Hesapla'),
+              ),
+              SizedBox(height: 20),
+              //if (result.isNotEmpty)
+                Text(
+                  'Sürtünme:  ${result.toStringAsFixed(2)} m',
+                  style: TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              /*Text(
+                    'Hesaplanan Verimlilik: ${result["system"] ?? "Hesaplanamadı"}'),*/
+              // Diğer sonuçları da görüntüleyebilirsiniz.
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+  class PumpEfficiency {
+  static const vis = [
+  [0, 1.791468],
+  [5, 1.517938],
+  [10, 1.306096],
+  [15, 1.138451],
+  [20, 1.003317],
+  [25, 0.892654],
+  [30, 0.800782],
+  [35, 0.723598],
+  [40, 0.658075],
+  [45, 0.601942],
+  [50, 0.553461],
+  [55, 0.511289],
+  [60, 0.474368],
+  [65, 0.441859],
+  [70, 0.413086],
+  [75, 0.387498],
+  [80, 0.364647],
+  [85, 0.344158],
+  [90, 0.325721],
+  [95, 0.309076],
+  [100, 0.294002],
+  [200, 0.294002]
+  ];
+
+  static Map<String, dynamic> calcReynold(
+  rho, u, innerDiameter, mu, vi) {
+  Map<String, dynamic> result = {};
+  double re = -1;
+
+  if (rho != null && u != null && innerDiameter != null && mu != null && mu != 0) {
+  re = (rho * u * innerDiameter / mu).round();
+  }
+  if (u != null && innerDiameter != null && vi != null) {
+  re = (u * innerDiameter / vi);
+  }
+
+  if (re > 4000) {
+  result["flow"] = "turbulans";
+  }
+  if (re > 2300 && re < 4000) {
+  result["flow"] = "turbulans - laminer";
+  }
+  if (re < 2300) {
+  result["flow"] = "laminer";
+  }
+  result["rn"] = re;
+
+  return result;
+  }
+
+  static double frictionCoefficient(
+  innerDiameter, k, re) {
+  double lambda = 0.08;
+  var leftL = 1 / math.sqrt(lambda);
+  var rightL = -2 * math.log10e * math.log(2.51 / (re * math.sqrt(lambda)) + k / innerDiameter / 3.72);
+
+  while (rightL - leftL >= 0) {
+  leftL = 1 / math.sqrt(lambda);
+  rightL = -2 * math.log10e * math.log(2.51 / (re * math.sqrt(lambda)) + k / innerDiameter / 3.72);
+
+  lambda = lambda - 0.0005;
+  }
+  return lambda;
+  }
+
+  static double calcVis(t3) {
+  if (t3 == null || t3 <= 0 || t3 > 100) {
+  return 0;
+  }
+
+  int i = vis.length;
+  for (var v = 0; v < vis.length; v++) {
+  if (t3 < vis[v][0]) {
+  i = v;
+  break;
+  }
+  }
+
+  var t1 = vis[i - 1][0];
+  var t2 = vis[i][0];
+  var v1 = vis[i - 1][1];
+  var v2 = vis[i][1];
+
+  var v3 = ((t2 - t3) * (v1 - v2)) / (t2 - t1) + v2;
+
+  return v3 / 1000000;
+  }
+
+  static Map<String, dynamic> calcColebrook(
+  innerDiameter, k, u, temperature) {
+  Map<String, dynamic> result = {};
+  result["vi"] = calcVis(temperature);
+
+  Map<String, dynamic> rey = calcReynold(0, u, innerDiameter, 0, result["vi"]);
+  result["ren"] = rey["rn"];
+  result["fd"] = frictionCoefficient(innerDiameter, k, result["ren"]);
+  result["rr"] = k / innerDiameter;
+
+  result["turb"] = rey["flow"];
+  return result;
+  }
+
+  static Map<String, dynamic> calcYield(
+  flow,
+  power,
+  innerDiameter,
+  k,
+  l,
+  temperature,
+  motorEfficiency,
+  waterLevel,
+  pressure,
+  exloss) {
+  var a = math.pi * math.pow(innerDiameter, 2) / 4;
+
+  var u = flow / 3600 / a;
+  Map<String, dynamic> result = calcColebrook(innerDiameter, k, u, temperature);
+
+  result["u"] = u;
+
+  try {
+  result["headLoss"] = (result["fd"] * l * math.pow(u, 2)) / (innerDiameter * (2 * 9.7905)) + exloss;
+  } catch (_) {
+  result["headLoss"] = exloss;
+  }
+
+  result["hm"] = (pressure * 10.197162 + waterLevel + result["headLoss"]);
+  result["hydraulic"] = (result["hm"] * flow) / (367.2 * power * motorEfficiency / 10000);
+  result["system"] = result["hydraulic"] * motorEfficiency / 100;
+
+  return result;
+  }
+  }
+
+  double calculateNhidrolik(double sicaklik, double debi, double cap, double uzunluk) {
+    // Hesaplama fonksiyonunu burada düzgün şekilde implement etmelisiniz.
+    // Bu sadece bir örnek fonksiyon.
+    double result = (debi * cap) / (sicaklik * uzunluk);
+    return result;
+  }
